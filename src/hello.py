7e9728043a404062aba1256f922fbd2e4ef63a99
David <sc@silvercrow.org>
2020-06-24 23:10:20 -0600

    New message

--- src/hello.py
+++ src/hello.py
@@ -24,33 +24,24 @@ try:
     epd.Clear(0xFF)
     
     # Drawing on the image
-    font15 = ImageFont.truetype(os.path.join(picdir, 'Poppins-SemiBold.ttf'), 15)
+    font40 = ImageFont.truetype(os.path.join(picdir, 'Poppins-SemiBold.ttf'), 40)
     font24 = ImageFont.truetype(os.path.join(picdir, 'Poppins-SemiBold.ttf'), 24)
     
     logging.info("1.Drawing on the image...")
     image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
     draw = ImageDraw.Draw(image)
     
-    draw.rectangle([(0,0),(50,50)],outline = 0)
-    draw.rectangle([(55,0),(100,50)],fill = 0)
-    draw.line([(0,0),(50,50)], fill = 0,width = 1)
-    draw.line([(0,50),(50,0)], fill = 0,width = 1)
-    draw.chord((10, 60, 50, 100), 0, 360, fill = 0)
-    draw.ellipse((55, 60, 95, 100), outline = 0)
-    draw.pieslice((55, 60, 95, 100), 90, 180, outline = 0)
-    draw.pieslice((55, 60, 95, 100), 270, 360, fill = 0)
-    draw.polygon([(110,0),(110,50),(150,25)],outline = 0)
-    draw.polygon([(190,0),(190,50),(150,25)],fill = 0)
-    draw.text((120, 60), 'e-Paper demo', font = font15, fill = 0)
-    draw.text((110, 90), u'微雪电子', font = font24, fill = 0)
+
+    draw.text((10, 50), 'David Balan', font = font40, fill = 0)
+    draw.text((70, 90), u'デビッド', font = font24, fill = 0)
     epd.display(epd.getbuffer(image))
     time.sleep(2)
     
     # read bmp file 
-    logging.info("2.read bmp file...")
-    image = Image.open(os.path.join(picdir, '2in13.bmp'))
-    epd.display(epd.getbuffer(image))
-    time.sleep(2)
+    #logging.info("2.read bmp file...")
+    #image = Image.open(os.path.join(picdir, '2in13.bmp'))
+    #epd.display(epd.getbuffer(image))
+   # time.sleep(2)
         
     # # partial update
     
